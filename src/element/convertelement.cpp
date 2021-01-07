d12e6027cb4ffc55b8f9d0655bc872b274f9457f
Simon De Backer <simondebacker@msn.com>
2021-01-07 22:24:55 +0100

    Removed Mod polygon Init Mod input

--- src/element/convertelement.cpp
+++ src/element/convertelement.cpp
@@ -26,22 +26,6 @@ ConvertElement::ConvertElement(LoadElement element)
     for (int var = 0; var < element.description().size(); ++var)
     {
         if (element.description().at(var).value("XML_ElementName").toString()
-            == "polygon")
-        {
-            // for polygon
-            // Attribute closed is gone in V0.8
-            QMap<QString, QVariant>         Element_data;
-            QMapIterator<QString, QVariant> data(element.description().at(var));
-            while (data.hasNext())
-            {
-                data.next();
-                if (data.key() != "closed")
-                { Element_data.insert(data.key(), data.value()); }
-            }
-            data_description.append(Element_data);
-        }
-        else if (
-            element.description().at(var).value("XML_ElementName").toString()
             == "terminal")
         {
             // for terminal
@@ -57,6 +41,81 @@ ConvertElement::ConvertElement(LoadElement element)
                 if (data.key() != "nameHidden" && data.key() != "number")
                 { Element_data.insert(data.key(), data.value()); }
             }
+            data_description.append(Element_data);
+        }
+        else if (
+            element.description().at(var).value("XML_ElementName").toString()
+            == "input")
+        {
+            // for input
+            // converted to dynamic text
+            // see 02fb84caec7ca4df682edf7e4bee2202ab1d785b
+            QMap<QString, QVariant>         Element_data;
+            QMapIterator<QString, QVariant> data(element.description().at(var));
+
+            Element_data.insert("XML_ElementName", "dynamic_text");
+            // Attribute uuid is new
+            Element_data.insert("uuid", QUuid::createUuid().toString());
+
+            Element_data.insert("rotation", "0");
+            Element_data.insert("text_from", "UserText");
+            Element_data.insert("text_width", "-1");
+            Element_data.insert("frame", "false");
+            Element_data.insert("Halignment", "AlignLeft");
+            Element_data.insert("Valignment", "AlignTop");
+            double size = 9, y = 0;
+            while (data.hasNext())
+            {
+                data.next();
+                if (data.key() == "text")
+                { Element_data.insert("text", data.value()); }
+                else if (data.key() == "size")
+                {
+                    size = data.value().toDouble();
+                    // converted size in font
+                    Element_data.insert(
+                        "font",
+                        "Sans Serif," + data.value().toString()
+                            + ",-1,5,50,0,0,0,0,0");
+                }
+                else if (data.key() == "x")
+                {
+                    Element_data.insert("x", data.value());
+                }
+                else if (data.key() == "y")
+                {
+                    y = data.value().toDouble();
+                }
+                else if (data.key() == "tagg" && data.value() == "label")
+                {
+                    Element_data.insert("info_name", data.value());
+                }
+            }
+            Element_data.insert("y", y - (size * 2 + 5) / 2);
+            Element_data.insert("z", 3);
+
+            /* <input
+             * x="-44"
+             * y="0"
+             * size="9" 23-18=5
+             * rotate="true"
+             * text="/"/>*/
+
+            /*<dynamic_text
+             * x="-44"
+             * z="3"
+             * font="Sans Serif,9,-1,5,50,0,0,0,0,0"
+             * text_from="UserText"
+             * Valignment="AlignTop"
+             * y="-11.5"
+             * rotation="0"
+             * text_width="-1"
+             * uuid="{858d8e0b-bfa1-486c-a56f-ae7aea701649}"
+             * frame="false"
+             * Halignment="AlignLeft" >
+             * <text>/</text>
+             * </dynamic_text>
+             */
             data_description.append(Element_data);
         }
         else
